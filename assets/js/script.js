cc0e0f0e0c0932155c4950bff2bfa482b84be1a0
Robert Taylor <rjt903@gmail.com>
2023-01-27 19:54:26 +0000

    Display current weatehr conditions on page

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,65 +3,83 @@ let searchButton = $("#search-button");
 let searchInput = $("#search-input");
 let searchHistory = $("#history");
 let clearButton = $("#clear-button");
-
+let weatherMain = $("#weather-main");
+let currentConditionIcon = $("#current-condition-image");
+let currentTemp = $("#current-temp");
+let currentWind = $("#current-wind");
+let currentHumidity = $("#current-humid");
 
 //functions
 function search(event) {
-    event.preventDefault()
-    //set city name to the user input in the search field
-    let cityName = searchInput.val().trim();
-    if (cityName === "") {
-        //if no city entered show error alert
-        alert("Please enter a city name");
-        return;
-    };
+  event.preventDefault();
+  //set city name to the user input in the search field
+  let cityName = searchInput.val().trim();
+  if (cityName === "") {
+    //if no city entered show error alert
+    alert("Please enter a city name");
+    return;
+  }
 
-    //run the API request with the city name to get current weather
-    $.ajax({
-      url: queryURLCurrent + cityName + apiKey,
-      method: "GET",
-    }).then(function (response) {
-      console.log(response);
-      clearButton.attr("class", "btn btn-danger")
-      saveSearch(cityName)
-    });
+  //run the API request with the city name to get current weather
+  $.ajax({
+    url: queryURLCurrent + cityName + apiKey,
+    method: "GET",
+  }).then(function (response) {
+    clearButton.attr("class", "btn btn-danger");
+    saveSearch(cityName);
+    renderCurrentWeather(response);
+  });
 
-    searchInput.val("");
+  searchInput.val("");
 }
 
 function saveSearch(city) {
-    //create new button to show previously made search
-    let newCity = $("<button>").text(city);
-    newCity.attr("class", "btn btn-info mt-2 saved-city");
-    //add dataset name to the button storing the city name
-    newCity.attr("data-name", city);
-    //append the button to the search history list
-    searchHistory.append(newCity);
+  //create new button to show previously made search
+  let newCity = $("<button>").text(city);
+  newCity.attr("class", "btn btn-info mt-2 saved-city");
+  //add dataset name to the button storing the city name
+  newCity.attr("data-name", city);
+  //append the button to the search history list
+  searchHistory.append(newCity);
 }
 
 function retrieveSearch(event) {
-    //fetch the saved city name from the data set
-    let savedCity = $(event.target).attr("data-name");
-    //fetch weather from API call using the saved city name
-    $.ajax({
-        url: queryURLCurrent + savedCity + apiKey,
-        method: "GET",
-      }).then(function (response) {
-        console.log(response);
-      });
+  //fetch the saved city name from the data set
+  let savedCity = $(event.target).attr("data-name");
+  //fetch weather from API call using the saved city name
+  $.ajax({
+    url: queryURLCurrent + savedCity + apiKey,
+    method: "GET",
+  }).then(function (response) {
+    console.log(response);
+  });
 }
 
-function renderCurrentWeather() {
+function renderCurrentWeather(response) {
+  console.log(response.main.temp);
+  console.log(response.main.humidity);
+  console.log(response.weather[0].description);
+  console.log(response.wind.speed);
+  console.log(response.weather[0].icon);
 
+  var tempInC = response.main.temp - 273.15;
+  var rounded = Math.round(tempInC * 10) / 10
+
+    weatherMain.attr("class", "col-lg-9 pb-3");
+    iconCode = response.weather[0].icon;
+    currentConditionIcon.attr("src", `http://openweathermap.org/img/wn/${iconCode}@2x.png`);
+    currentTemp.text(rounded);
+    currentHumidity.text(response.main.humidity);
+    currentWind.text(response.wind.speed);
 }
 
 //event listeners
 searchButton.on("click", search);
 searchHistory.on("click", ".saved-city", retrieveSearch);
 clearButton.on("click", function (event) {
-    event.preventDefault();
-    //remove all saved searches
-    searchHistory.empty();
-    //hide the clear button when the search history is empty
-    clearButton.attr("class", "btn btn-danger d-none")
+  event.preventDefault();
+  //remove all saved searches
+  searchHistory.empty();
+  //hide the clear button when the search history is empty
+  clearButton.attr("class", "btn btn-danger d-none");
 });
