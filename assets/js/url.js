178a30fe2d4dcafd293244c45b7a7bd33fc728df
Robert Taylor <rjt903@gmail.com>
2023-01-27 19:09:47 +0000

    Fixed URL contrsuctor for current weatehr request

--- assets/js/url.js
+++ assets/js/url.js
@@ -1,4 +1,3 @@
 var cityName = "London";
-// var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey;
-var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=3d59fc1e00462c4c70bc7bfe51702416`;
-// var apiKey = "3d59fc1e00462c4c70bc7bfe51702416";
+var queryURLCurrent = `https://api.openweathermap.org/data/2.5/weather?q=`;
+var apiKey = "&appid=3d59fc1e00462c4c70bc7bfe51702416";
