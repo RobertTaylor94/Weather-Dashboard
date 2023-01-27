3f259a10e57a05cb7d4220b28a573861ba12f4a3
Robert Taylor <rjt903@gmail.com>
2023-01-27 19:25:58 +0000

    Added button to clear search history

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@
 let searchButton = $("#search-button");
 let searchInput = $("#search-input");
 let searchHistory = $("#history");
+let clearButton = $("#clear-button");
 
 
 //functions
@@ -21,6 +22,7 @@ function search(event) {
       method: "GET",
     }).then(function (response) {
       console.log(response);
+      clearButton.attr("class", "btn btn-danger")
       saveSearch(cityName)
     });
 
@@ -28,14 +30,19 @@ function search(event) {
 }
 
 function saveSearch(city) {
+    //create new button to show previously made search
     let newCity = $("<button>").text(city);
     newCity.attr("class", "btn btn-info mt-2 saved-city");
+    //add dataset name to the button storing the city name
     newCity.attr("data-name", city);
+    //append the button to the search history list
     searchHistory.append(newCity);
 }
 
 function retrieveSearch(event) {
+    //fetch the saved city name from the data set
     let savedCity = $(event.target).attr("data-name");
+    //fetch weather from API call using the saved city name
     $.ajax({
         url: queryURLCurrent + savedCity + apiKey,
         method: "GET",
@@ -44,6 +51,17 @@ function retrieveSearch(event) {
       });
 }
 
+function renderCurrentWeather() {
+
+}
+
 //event listeners
 searchButton.on("click", search);
 searchHistory.on("click", ".saved-city", retrieveSearch);
+clearButton.on("click", function (event) {
+    event.preventDefault();
+    //remove all saved searches
+    searchHistory.empty();
+    //hide the clear button when the search history is empty
+    clearButton.attr("class", "btn btn-danger d-none")
+});
