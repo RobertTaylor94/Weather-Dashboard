54ec4c5bd7b6b70c49f90f2beb5c1fe1a2c5d1a9
Robert Taylor <rjt903@gmail.com>
2023-01-28 14:11:46 +0000

    Disabled switch statement

--- assets/js/background.js
+++ assets/js/background.js
@@ -1,27 +1,31 @@
-function updateBackground(response) {
-    let conditionMain = response.weather[0].main;
+// let body = $("body");
+// let todayCard = $(".card-body");
 
-    switch (conditionMain) {
-        case "Thunderstorm":
-            console.log("Thunderstorm");
-            break;
-        case "Drizzle":
-            console.log("Drizzle");
-            break;
-        case "Rain":
-            console.log("Rain");
-            break;
-        case "Snow":
-            console.log("Snow");
-            break;
-        case "Clear":
-            console.log("Clear");
-            break;
-        case "Clouds":
-            console.log("Clouds");
-            break;
-        default:
-            console.log("no weather main");
-            break;
-    }
-};
+// function updateBackground(response) {
+//     let conditionMain = response.weather[0].main;
+
+//     switch (conditionMain) {
+//         case "Thunderstorm":
+//             console.log("Thunderstorm");
+//             break;
+//         case "Drizzle":
+//             console.log("Drizzle");
+//             break;
+//         case "Rain":
+//             console.log("Rain");
+//             break;
+//         case "Snow":
+//             console.log("Snow");
+//             break;
+//         case "Clear":
+//             console.log("Clear");
+//             break;
+//         case "Clouds":
+//             console.log("Clouds");
+//             todayCard.css("background-color","yellow")
+//             break;
+//         default:
+//             console.log("no weather main");
+//             break;
+//     }
+// };
