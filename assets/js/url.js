797d4cc72d1011ab41a41e790a2cd3cac854f333
Robert Taylor <rjt903@gmail.com>
2023-01-28 14:11:10 +0000

    Updated API call URL

--- assets/js/url.js
+++ assets/js/url.js
@@ -1,5 +1,3 @@
 var cityName = "London";
-var queryURLCurrent = `https://api.openweathermap.org/data/2.5/weather?q=`;
+var queryURLCurrent = `https://api.openweathermap.org/data/2.5/forecast?q=`;
 var apiKey = "&appid=3d59fc1e00462c4c70bc7bfe51702416";
-var iconURL = `http://openweathermap.org/img/wn/${iconCode}@2x.png`
-var iconCode = ""
